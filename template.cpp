a4c4ab05118acbcb2b87e4ee105dd4b86a699edf
ItzAkmal <56719270+ItzAkmal@users.noreply.github.com>
2020-03-29 17:38:45 +0600

    template.cpp added

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define scd(t) scanf("%d", &t)
+#define scld(t) scanf("%ld", &t)
+#define sclld(t) scanf("%lld", &t)
+#define scf(t) scanf("%f", &t)
+#define sclf(t) scanf("%lf", &t)
+#define scc(t) scanf("%c", &t)
+#define scs(t) getline(cin, t)
+#define forn(t) for (int i = 0; i < t; i++)
+#define mp make_pair
+#define pb push_back
+#define PI 3.1415926535897932384626433832795
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<int, int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
